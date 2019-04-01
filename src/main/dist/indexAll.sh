7d5cd1526aeb8a4f74cbeaadb4e2f58a1d5c7a41
Marek Tutaj <mtutaj@mcw.edu>
2019-04-01 12:19:35 -0500

    fixed logging

--- src/main/dist/indexAll.sh
+++ src/main/dist/indexAll.sh
@@ -1,5 +1,13 @@
 # generate full annot index for all public ontologies
 #
 APPHOME=/home/rgddata/pipelines/fullAnnotIndex
+SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
+EMAIL_LIST=mtutaj@mcw.edu
+if [ "$SERVER" == "REED" ]; then
+  EMAIL_LIST=rgd.developers@mcw.edu
+fi
 
 $APPHOME/index.sh "*"
+
+mailx -s "[$SERVER] Output from full annot index pipeline" $EMAIL_LIST < $APPHOME/logs/core.log
+
